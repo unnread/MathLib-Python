371962ce3859db8040238cb3224b2aff592865b5
YDev+ <telmouneyouness@gmail.com>
2026-07-31 13:33:24 +0200

    Add files via upload

---
+++ MathLib/__init__.py
@@ -0,0 +1,18 @@
+# __init__.py
+
+from .calcul import (
+    additionner,
+    soustraire,
+    multiplier,
+    diviser,
+    carre,
+    puissance,
+    racine,
+    pythagoreH,
+    pythagoreD,
+    sin,
+    cos,
+    tan,
+    degresVersRadians,
+    radiansVersDegres
+)
